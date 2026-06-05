7e0ad75d2d3077a336d69e6fa8e0bbb8d59b4600
stefanny223 <stefanny24si@mahasiwa.pcr.ac.id>
2026-06-05 14:49:45 +0700

    ubah

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,11 +19,6 @@ export default function Header() {
                focus:bg-white transition"
         />
 
-        <FaSearch
-          id="search-icon"
-          className="absolute right-3 top-1/2 transform -translate-y-1/2 
-               text-gray-400 text-sm pointer-events-none"
-        />
       </div>
 
       {/* Icon & Profile Section */}
